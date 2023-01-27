c5fa7187cebb3748a4b2e375ec576c248221adc4
Taisa Braga <taisabraga.adm@gmail.com>
2023-01-27 15:40:41 -0300

    refact api call

---
+++ src/hooks/useGetCharacters.ts
@@ -0,0 +1,75 @@
+import { gql, LazyQueryResult, OperationVariables, useLazyQuery, useQuery } from '@apollo/client'
+import { ApolloError } from 'apollo-server';
+
+export const GET_CHARACTERS = gql`
+  query Characters($page: IFilterPage){
+    characters(page: $page){
+      results{
+        id
+        image,
+        name,
+        species,
+        status,
+        type,
+      },
+      info {
+        next,
+        pages,
+        prev,
+      }
+    }
+  }
+`
+
+export interface IFilterPage {
+  page: number;
+}
+
+export interface Info {
+  next: number
+  pages: number
+  prev: number
+}
+
+export interface ICharacter {
+  results: object[]
+  id: number,
+  image: string,
+  name: string,
+  species: string,
+  status: string,
+  type: string,
+  info: Info,
+}
+
+export interface IGetCharacters {
+  characters: {
+    result: ICharacter
+  }
+}
+
+export type Characters = (
+  page: IFilterPage
+) => Promise<LazyQueryResult<IGetCharacters, OperationVariables>>
+
+export type useGetCharactersResult = {
+  characters: Characters;
+  loading: boolean;
+  error: null | ApolloError;
+  data: IGetCharacters | null | undefined;
+}
+
+export const useGetCharacters = () => {
+  const [getQuery, { data, loading, error, fetchMore, refetch }] = useLazyQuery<IGetCharacters>(GET_CHARACTERS);
+
+  const getCharactersResult = async (
+    page: IFilterPage
+  ) => {
+    return await getQuery({
+      variables: { page },
+    });
+  };
+  return { getCharactersResult, data, loading, error, fetchMore, refetch }
+}
+
+export default useGetCharacters;
