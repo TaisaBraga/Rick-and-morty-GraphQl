91e6ec75edc3c17eec9a4d6c5024f3813fc5e66b
Taisa Braga <taisabraga.adm@gmail.com>
2023-09-29 15:13:23 -0300

    deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Rick-and-morty-GraphQl/",
   test: {
     globals: true,
     setupFiles: 'src/setupTests.js',
